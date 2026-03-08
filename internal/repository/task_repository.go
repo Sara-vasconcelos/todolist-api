9a1b1477764900a9bc3e835b020f6405f955e394
Sara Vasconcelos <vasconcelossara11@gmail.com>
2026-03-08 16:59:49 -0300

    create task model

---
+++ internal/repository/task_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import "time"
+
+type Task struct{
+
+	ID          string    `json:"id" bson:"_id,omitempty"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	Priority    string    `json:"priority"`
+	DueDate     time.Time `json:"due_date"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
